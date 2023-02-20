ca397f5a8e27fee25f0e819301f84687b1d612f7
nick <mwauranicholas10@gmail.com>
2023-02-20 15:16:59 +0300

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,23 @@
-require 'pry'
-
 class String
 
   def sentence?
-    
-  end
+    self[-1] == "."
+
+  end  
+  
+
 
   def question?
+    self[-1] == "?"
 
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    self.split(/[.?!]/).delete_if { |sentence| sentence.size < 2 }.size
   end
+
 end
